29f190126cee9b935a58e7db7bea82f918dc5134
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-05-12 23:20:27 +0900

    Initialized firebase in app

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVstIqjEOq3xYlom7sMy4QQo157mjwvb4",
+  authDomain: "minsapay.firebaseapp.com",
+  projectId: "minsapay",
+  storageBucket: "minsapay.appspot.com",
+  messagingSenderId: "776396681088",
+  appId: "1:776396681088:web:ff6eaa5cc690170e07db65"
+};
+
+const app = initializeApp(firebaseConfig);
