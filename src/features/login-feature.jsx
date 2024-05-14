401a11c6f4166af49bfbbd0fae479a6840947e81
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-05-14 12:42:14 +0900

    Add husky and other tools

--- src/features/login-feature.jsx
+++ src/features/login-feature.jsx
@@ -1,81 +1,82 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, getDocs, /*orderBy,*/ query } from "firebase/firestore";
 import { database } from "../firebase";
-import cryptoJS from 'crypto-js'
+import cryptoJS from "crypto-js";
 
 function isStudent(userID) {
-    switch(userID[0]) {
-        case '0':
-            return true;
-        case '1':
-            return true;
-        case '2':
-            return true;
-        case '3':
-            return true;
-        case '4':
-            return true;
-        case '5':
-            return true;
-        case '6':
-            return true;
-        case '7':
-            return true;
-        case '8':
-            return true;
-        case '9':
-            return true;
-        default:
-            return false;
-    }
+  switch (userID[0]) {
+    case "0":
+      return true;
+    case "1":
+      return true;
+    case "2":
+      return true;
+    case "3":
+      return true;
+    case "4":
+      return true;
+    case "5":
+      return true;
+    case "6":
+      return true;
+    case "7":
+      return true;
+    case "8":
+      return true;
+    case "9":
+      return true;
+    default:
+      return false;
+  }
 }
 
 export const auth = {
-    currentUser: null,
-    error: "",
-    async signIn(userID, password) {
-        if(isStudent(userID)) {
-            // 학생 로그인
-            const studentsQuery = query(
-                collection(database, "Students")
-            );
-            const students = await getDocs(studentsQuery)
-            let documentIndex = -1;
-            for(let i=0;i<students.docs.length;i++) { // 학생 아이디 대조
-                if(userID === students.docs[i].id) {
-                    documentIndex = i;
-                    break;
-                }
-            }
-        if(documentIndex === -1) { this.error = "아이디가 존재하지 않습니다."; return;}
-        const user = students.docs[documentIndex].data();
-        if(cryptoJS.SHA256(password).toString()  === user.password) { // 학생 비번 대조
-            console.log("Approved as Buyer"); // Buyer로 로그인
-        } else {
-            const teamsQuery = query(
-                collection(database, "Teams")
-            );
-            const teams = await getDocs(teamsQuery)
-            const encryptedPassword = cryptoJS.SHA256(password).toString();
-            documentIndex = -1;
-            for(let i=0;i<teams.docs.length;i++) { // 부스 비번 대조
-                if(encryptedPassword === teams.docs[i].data().password) {
-                    documentIndex = i;
-                    console.log("Approved as Seller") // Seller로 로그인
-                    break;
-                }
-            }
-            if (documentIndex === -1) { this.error = '비밀번호를 잘못 입력하셨습니다.'; return }
+  currentUser: null,
+  error: "",
+  async signIn(userID, password) {
+    if (isStudent(userID)) {
+      // 학생 로그인
+      const studentsQuery = query(collection(database, "Students"));
+      const students = await getDocs(studentsQuery);
+      let documentIndex = -1;
+      for (let i = 0; i < students.docs.length; i++) {
+        // 학생 아이디 대조
+        if (userID === students.docs[i].id) {
+          documentIndex = i;
+          break;
         }
-        
-        } 
-        else {
-            // 부스 로그인
-            console.log("I am team");
-
+      }
+      if (documentIndex === -1) {
+        this.error = "아이디가 존재하지 않습니다.";
+        return;
+      }
+      const user = students.docs[documentIndex].data();
+      if (cryptoJS.SHA256(password).toString() === user.password) {
+        // 학생 비번 대조
+        console.log("Approved as Buyer"); // Buyer로 로그인
+      } else {
+        const teamsQuery = query(collection(database, "Teams"));
+        const teams = await getDocs(teamsQuery);
+        const encryptedPassword = cryptoJS.SHA256(password).toString();
+        documentIndex = -1;
+        for (let i = 0; i < teams.docs.length; i++) {
+          // 부스 비번 대조
+          if (encryptedPassword === teams.docs[i].data().password) {
+            documentIndex = i;
+            console.log("Approved as Seller"); // Seller로 로그인
+            break;
+          }
         }
-
-    },
-}
+        if (documentIndex === -1) {
+          this.error = "비밀번호를 잘못 입력하셨습니다.";
+          return;
+        }
+      }
+    } else {
+      // 부스 로그인
+      console.log("I am team");
+    }
+  },
+};
 
 /* 
 const doc = await addDoc(collection(db, "tweets"), {
