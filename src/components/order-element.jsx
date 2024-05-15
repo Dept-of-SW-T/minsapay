4b95e8644b5c90657d7a0a26d35594be74bdc009
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-05-15 12:36:14 +0900

    add order list

--- src/components/order-element.jsx
+++ src/components/order-element.jsx
@@ -6,13 +6,17 @@ const Wrapper = styled.div`
   border: 1px solid black;
   border-radius: 20px;
   justify-content: space-around;
+  width: 50%;
+  background-color: red;
+  padding-left: 0.5%;
+  padding-right: 0.5%;
 `;
 
 const Text = styled.div`
   text-align: center;
   font-size: x-large;
-  margin-left: 3px;
-  margin-right: 3px;
+  flex: auto;
+  background-color: white;
 `;
 
 export default function OrderElement({ menuName, userName, time, status }) {
