80687fd54c7f99c75fdf32a3d742cdaf25570080
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-05-15 11:36:51 +0900

    Modify order element

--- src/components/order-element.jsx
+++ src/components/order-element.jsx
@@ -2,15 +2,26 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  flex-direction: row;
+  border: 1px solid black;
+  border-radius: 20px;
+  justify-content: space-around;
+`;
+
+const Text = styled.div`
+  text-align: center;
+  font-size: x-large;
+  margin-left: 3px;
+  margin-right: 3px;
 `;
 
 export default function OrderElement({ menuName, userName, time, status }) {
   return (
     <Wrapper>
-      <div>{menuName}</div>
-      <div>{userName}</div>
-      <div>{time}</div>
-      <div>{status}</div>
+      <Text>{menuName}</Text>
+      <Text>{userName}</Text>
+      <Text>{time}</Text>
+      <Text>{status}</Text>
     </Wrapper>
   );
 }
