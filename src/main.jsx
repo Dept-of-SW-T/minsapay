7e3182afb046529d6f912e3b296b9653abd88330
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-05-25 22:24:35 +0900

    Returned to strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-//import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />,
-  /* <React.StrictMode>
+  <React.StrictMode>
     <App />
-  </React.StrictMode>, */
+  </React.StrictMode>,
 );
