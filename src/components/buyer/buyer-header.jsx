cc589490d0dc383a4f0646a18365f2c0688b1e7a
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-06-02 19:39:54 +0900

    Added Kiosk cover image feature

--- src/components/buyer/buyer-header.jsx
+++ src/components/buyer/buyer-header.jsx
@@ -41,7 +41,7 @@ export function BuyerHeader({ balance }) {
   const onLogoClick = (e) => {
     // logo 누르면 홈으로 navigate
     e.preventDefault();
-    navigate("../cpu-home");
+    navigate("../buyer-home");
   };
   const onLogOutIconClick = async (e) => {
     // logout 누르면 confirm 띄우고 로그아웃 후 home으로 navigate --> 저절로 logout화면으로 protected routes를 통해 연결
--- src/routes/kiosk/kiosk-cover.jsx
+++ src/routes/kiosk/kiosk-cover.jsx
@@ -1,67 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { kioskFirebase } from "../../features/kiosk-firebase-interaction";
+import { CPUFirebase } from "../../features/CPU-firebase-interaction";
 import { onSnapshot } from "firebase/firestore";
-import kioskImage from "../../images/ChangeKioskImage.svg"; //부스 이미지가 뭔지 모르겠어서 우선 대충 이거 넣어놓음. 부스 개별 사진으로 바꿔줄 것.
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh; // 전체 화면 높이 설정
+  width: 100vw;
+`;
+const KioskImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
 
 export default function KioskCover() {
   // 지금 꺼 전체적으로 수정
+  const navigate = useNavigate();
+  const [kioskImageDownloadUrl, setKioskImageDownloadUrl] = useState("");
   useEffect(() => {
     let unsubscribe = null;
     const init = async () => {
-      await kioskFirebase.init();
+      await CPUFirebase.init();
+      await CPUFirebase.kioskImageInit();
+      setKioskImageDownloadUrl(CPUFirebase.kioskImageDownloadUrl);
     };
     init();
-    unsubscribe = onSnapshot(kioskFirebase.userDocRef, (doc) => {
-      kioskFirebase.userDocData = doc.data();
-      if (kioskFirebase.userDocData.linked_buyer !== "")
+    unsubscribe = onSnapshot(CPUFirebase.userDocRef, async (doc) => {
+      CPUFirebase.userDocData = doc.data();
+      await CPUFirebase.kioskImageInit();
+      setKioskImageDownloadUrl(CPUFirebase.kioskImageDownloadUrl);
+      if (CPUFirebase.userDocData.linked_buyer !== "")
         navigate("../kiosk-home");
     });
     return () => {
       unsubscribe && unsubscribe();
     };
   }, []);
-  const navigate = useNavigate();
-
-  const kioskCoverStyle = {
-    display: "flex",
-    flexDirection: "column",
-    gap: "10px",
-    backgroundImage: `url(${kioskImage})`, // 배경 이미지 설정
-    backgroundSize: "cover", // 이미지가 요소를 덮도록 설정
-    backgroundPosition: "center", // 이미지를 중앙에 위치시키기
-    height: "100vh", // 전체 화면 높이 설정
-    width: "100vw",
-  };
-
-  /*return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
-      <div>kiosk cover</div>
-      <button
-        onClick={() => {
-          navigate("../kiosk-home/kiosk-authentication");
-        }}
-      >
-        계속하기 {">>"}
-      </button>
-    </div>
-  );*/
-  /*return (
-    <div style={kioskCoverStyle}>
-      <div>kiosk cover</div>
-      <button
-        onClick={() => {
-          navigate("../kiosk-home/kiosk-authentication");
-        }}
-      >
-        계속하기 {">>"}
-      </button>
-    </div>
-  );*/
-
   const handleClick = () => {
     navigate("../kiosk-home/kiosk-authentication");
   };
 
-  return <div style={kioskCoverStyle} onClick={handleClick}></div>;
+  return (
+    <Wrapper>
+      <KioskImage src={kioskImageDownloadUrl} onClick={handleClick} />
+    </Wrapper>
+  );
 }
