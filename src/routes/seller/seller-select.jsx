e3e7ed1cade76fe471c0d7929988991529b4dfde
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2024-06-06 22:29:09 +0900

    Set seller-select css

--- src/routes/seller/seller-select.jsx
+++ src/routes/seller/seller-select.jsx
@@ -3,14 +3,31 @@ import { useEffect, useState } from "react";
 import TeamList from "../../components/seller/team-list";
 import styled from "styled-components";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100%;
+`;
+
+const TeamElement = styled.div`
+  width: 100%;
+  height: 10%;
+  text-align: center;
+`;
 
 export default function SellerSelect() {
   const [teamList, setTeamList] = useState([]);
   useEffect(() => {
     const init = async () => {
       await sellerFirebase.init();
-      setTeamList(sellerFirebase.userDocData.team_list);
+      setTeamList(
+        sellerFirebase.userDocData.team_list.map((val, index) => (
+          <TeamElement key={index}>{val}</TeamElement>
+        )),
+      );
     };
     init();
   }, []);
