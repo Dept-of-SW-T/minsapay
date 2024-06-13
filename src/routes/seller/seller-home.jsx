b590e6a8012564754b39694092da7cea9dd936ba
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-06-13 16:43:01 +0900

    Added sync to seller home / please edit seller order element / it just is REALLY REALLY faulty, needs css, and once it finishes its cycle, the change state button stops

--- src/routes/seller/seller-home.jsx
+++ src/routes/seller/seller-home.jsx
@@ -5,6 +5,7 @@ import { sellerFirebase } from "../../features/seller-firebase-interaction";
 import OrderElementSeller from "../../components/seller/order-element-seller";
 import CoupleList from "../../components/CPU/couple-list";
 import { SellerHeader } from "../../components/seller/seller-header";
+import { onSnapshot } from "firebase/firestore";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -17,6 +18,7 @@ export default function SellerHome() {
   const [orderList, setOrderList] = useState([]);
   const currentTeam = useParams().id;
   useEffect(() => {
+    let unsubscribe = null;
     const init = async () => {
       await sellerFirebase.init();
       await sellerFirebase.getTeamData(currentTeam);
@@ -37,6 +39,31 @@ export default function SellerHome() {
       );
     };
     init();
+    unsubscribe = onSnapshot(sellerFirebase.userDocRef, (doc) => {
+      sellerFirebase.userDoc = doc;
+      sellerFirebase.userDocData = doc.data();
+      sellerFirebase.orderHistory = JSON.parse(
+        sellerFirebase.userDocData.order_history,
+      );
+      setOrderList(
+        sellerFirebase.orderHistory
+          .toReversed()
+          .map((val, index) => (
+            <OrderElementSeller
+              menuName={val.menu_name}
+              status={val.current_state}
+              id={val.order_id}
+              processor={val.order_processor}
+              buyer={val.buyer_name}
+              receptionTime={val.reception_time}
+              key={index}
+            />
+          )),
+      );
+    });
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
