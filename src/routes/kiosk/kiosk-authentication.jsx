15d3c8acc5ce04a74f4af57c21320ee17f4b9140
lgc02161 <lgc02161@gmail.com>
2024-07-05 11:25:33 +0900

    changed gradation and last page of kiosk to kmla picture

--- src/routes/kiosk/kiosk-authentication.jsx
+++ src/routes/kiosk/kiosk-authentication.jsx
@@ -2,6 +2,24 @@ import { useEffect, useState } from "react";
 import { kioskFirebase } from "../../features/kiosk-firebase-interaction";
 import { useNavigate } from "react-router-dom";
 import { onSnapshot } from "firebase/firestore";
+import styled from "styled-components";
+import backgroundImagePhoneScreen from "../../images/bg-candidate-phone-screen.png";
+import backgroundImageWideScreen from "../../images/bg-candidate-wide-screen.jpg";
+
+const OuterWrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url(${backgroundImageWideScreen});
+  @media only screen and (max-width: 768px) {
+    background-image: url(${backgroundImagePhoneScreen});
+    background-size: cover;
+  }
+`;
 
 export default function KioskAuthentication() {
   const navigate = useNavigate();
@@ -28,27 +46,29 @@ export default function KioskAuthentication() {
   }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100vw",
-        background: "radial-gradient(circle, #2b81ea, #06195e)", // 파란색 그라데이션- 색 수정 필요
-        flexDirection: "column",
-        gap: "10px",
-      }}
-    >
+    <OuterWrapper>
       <div
         style={{
-          fontSize: "6.0vw",
-          color: "white", // 글씨 색상
-          fontFamily: "Century Gothic",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+          background: "rgba(0,0,0,0.5)",
+          flexDirection: "column",
+          gap: "10px",
         }}
       >
-        {kioskAuthenticationNumber}
+        <div
+          style={{
+            fontSize: "6.0vw",
+            color: "white", // 글씨 색상
+            fontFamily: "Century Gothic",
+          }}
+        >
+          {kioskAuthenticationNumber}
+        </div>
       </div>
-    </div>
+    </OuterWrapper>
   );
 }
