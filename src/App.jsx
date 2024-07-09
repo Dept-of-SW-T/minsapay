ff7d962e70a04d3013b4ea0c27e8bf7e43bb14c1
Yean Lee <talk2yeanlee@gmail.com>
2024-07-09 22:56:34 +0900

    Changed app.js moderator import

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ import DeveloperHome from "./developer/developer-home";
 import SellerSelect from "./routes/seller/seller-select";
 import SellerHome from "./routes/seller/seller-home";
 import BuyerSetting from "./routes/buyer/buyer-setting";
-import ModeratorHome from "./routes/Moderator/moderator-home";
+import ModeratorHome from "./routes/moderator/moderator-home";
 import LogHome from "./routes/log/log-home";
 
 const router = createBrowserRouter([
