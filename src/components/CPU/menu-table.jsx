2bccc4d94795676eeda548f1befc83ab294ae530
Yean Lee <talk2yeanlee@gmail.com>
2024-07-12 00:40:58 +0900

    fixed CPU home table design

--- src/components/CPU/menu-table.jsx
+++ src/components/CPU/menu-table.jsx
@@ -1,25 +1,56 @@
 import styled from "styled-components";
-import { MINSAPAY_TITLE } from "../theme-definition";
+import { MINSAPAY_TITLE, MINSAPAY_BLUE } from "../theme-definition";
+
+const Table = styled.table`
+  width: 90%;
+  max-width: 100vw;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  border-collapse: collapse;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  font-family: ${MINSAPAY_TITLE};
+`;
+
+const Th = styled.th`
+  padding: 10px;
+  border: 1px solid #ccc;
+  background-color: #f0f0f0;
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+  background-color: ${MINSAPAY_BLUE};
+  color: white;
+  padding: 10px;
+  border: 1px solid #ccc;
+  text-align: center;
+`;
+
+const Td = styled.td`
+  padding: 10px;
+  border: 1px solid #ccc;
+  font-size: 14px;
+  text-align: center;
+  vertical-align: middle;
+`;
+
+const RefundButton = styled.button`
+  padding: 5px 10px;
+  font-size: 12px;
+  color: white;
+  background-color: #f44336;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin: 0 5px;
+  text-align: center;
+
+  &:hover {
+    background-color: #e53935;
+  }
+`;
 
 export default function MenuTable({ orderList }) {
-  const Table = styled.table`
-    width: 90%;
-    max-width: 100vw;
-    margin-top: 50px;
-    margin-bottom: 50px;
-    border-collapse: collapse;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    font-family: ${MINSAPAY_TITLE};
-  `;
-
-  const Th = styled.th`
-    padding: 10px;
-    border: 1px solid #ccc;
-    background-color: #f0f0f0;
-    font-size: 14px;
-  `;
-
   return (
     <Table>
       <thead>
@@ -32,7 +63,20 @@ export default function MenuTable({ orderList }) {
           <Th>환불</Th>
         </tr>
       </thead>
-      <tbody>{orderList}</tbody>
+      <tbody>
+        {orderList.map((order, index) => (
+          <tr key={index}>
+            <Td>{order.id}</Td>
+            <Td>{order.menu_name}</Td>
+            <Td>{order.buyer_name}</Td>
+            <Td>{order.reception_time}</Td>
+            <Td>{order.current_state}</Td>
+            <Td>
+              <RefundButton>Refund</RefundButton>
+            </Td>
+          </tr>
+        ))}
+      </tbody>
     </Table>
   );
 }
--- src/routes/CPU/cpu-home.jsx
+++ src/routes/CPU/cpu-home.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
-// import CoupleList from "../../components/CPU/couple-list";
 import { CPUHeader } from "../../components/CPU/cpu-header";
 import { useNavigate } from "react-router-dom";
 import ChangeKioskImage from "../../images/ChangeKioskImage.svg";
@@ -11,16 +10,13 @@ import {
   MINSAPAY_TITLE,
 } from "../../components/theme-definition";
 import { CPUFirebase } from "../../features/CPU-firebase-interaction";
-// import OrderElementCPU from "../../components/CPU/order-element-CPU";
 import { onSnapshot } from "firebase/firestore";
 import Loading from "../../components/loading";
-import MenuTable from "../../components/CPU/menu-table";
-
-// border 다 추가하기
+import MenuTable from "../../components/CPU/menu-table"; // Import MenuTable component
 
 const Wrapper = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: flex;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -29,7 +25,6 @@ const CPUHomeBox = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-  //margin-bottom: 20px;
 `;
 const TopDiv = styled.div`
   width: 90%;
@@ -44,23 +39,19 @@ const TopDiv = styled.div`
   justify-content: space-evenly;
   font-family: ${MINSAPAY_TITLE};
   gap: 4%;
-  margin: 4vh auto 5vh;
+  margin: 4vh auto 2vh;
   @media only screen and (max-width: 2000px) {
     font-size: calc(0.05vw + 1.1em);
   }
-  // font size change needed
 `;
 const Title = styled.div`
-  /* width: 969px; */
   flex: 2 1 0;
   height: 100%;
-  //border: 3px solid ${BORDER_GRAY};
   border-radius: 10px;
   background-size: cover;
   background-position: center;
 `;
 const OpacityLayer = styled.div`
-  // 글씨가 세로로 정중앙이 아님 고침이 필요
   width: 100%;
   height: 100%;
   border-radius: 17px;
@@ -74,31 +65,23 @@ const OpacityLayer = styled.div`
   background-color: rgb(0, 0, 0, 0.3);
 `;
 const TeamName = styled.div`
-  /* margin-left: 125px; */
   text-align: center;
-  /* width: 240px; */
   flex: 2 1 0;
 `;
 const Balance = styled.div`
-  // needs font change
-  /* margin-left: 190px; */
   text-align: center;
-  /* width: 280px; */
   z-index: 100;
   flex: 2 1 0;
 `;
 const Label = styled.label`
-  /* height: 40px; */
   flex: 1 1 0;
   display: flex;
   justify-content: center;
 `;
 const Image = styled.img`
-  background-color: ${BACKGROUND_GRAY}; // 색 바꿀 것
+  background-color: ${BACKGROUND_GRAY};
   border-radius: 10px;
-  /* width: 40px; */
   width: 1.1em;
-  /* margin-left: 15px; */
   aspect-ratio: 1;
   &:hover {
     opacity: 0.6;
@@ -109,13 +92,11 @@ const ImageUpload = styled.input`
   display: none;
 `;
 const HeaderBtns = styled.div`
-  // button 사이 gap 추가
   display: flex;
   flex-direction: column;
   @media only screen and (max-width: 768px) {
     flex-direction: row;
   }
-  /* width: 384px; */
   flex: 1 1 0;
   height: 100%;
   justify-content: space-between;
@@ -125,7 +106,6 @@ const Btn = styled.button`
   border-radius: 10px;
   border: 0px solid ${BORDER_GRAY};
   background-color: ${MINSAPAY_BLUE};
-
   color: white;
   text-align: center;
   width: 100%;
@@ -139,44 +119,14 @@ const Btn = styled.button`
     opacity: 0.8;
   }
 `;
-const BodyDiv = styled.div`
-  /* margin-top: 5vh; */
-  width: 100%;
-  height: 59vh;
-  @media only screen and (max-width: 768px) {
-    height: 56vh;
-  }
+const TableWrapper = styled.div`
+  width: 100vw;
+  height: flex;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
 `;
 
-const Td = styled.td`
-  padding: 10px;
-  border: 1px solid #ccc;
-  font-size: 14px;
-  text-align: center; /* 텍스트 가운데 정렬 */
-  vertical-align: middle; /* 세로 가운데 정렬 */
-`;
-
-const RefundButton = styled.button`
-  padding: 5px 10px;
-  font-size: 12px;
-  color: white;
-  background-color: #f44336;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  margin: 0 5px;
-  text-align: center; /* 버튼 가운데 정렬 */
-
-  &:hover {
-    background-color: #e53935;
-  }
-`;
-
-// add isloading to total page and image upload
 export default function CPUHome() {
   const [balance, setBalance] = useState(0);
   const [kioskImage, setKioskImage] = useState("khgyhgjhfg");
@@ -185,27 +135,21 @@ export default function CPUHome() {
   useEffect(() => {
     let unsubscribe = null;
     const init = async () => {
-      // 초기에 잔고, 이미지 로딩
       await CPUFirebase.init();
       await CPUFirebase.kioskImageInit();
       setIsLoading(false);
       setBalance(CPUFirebase.userDocData.balance);
       setKioskImage(CPUFirebase.kioskImageDownloadUrl);
       setOrderList(
-        CPUFirebase.orderHistory.toReversed().map((val, index) => (
-          <tr key={index}>
-            <Td>{val.menu_name}</Td>
-            <Td>{val.buyer_name}</Td>
-            <Td>{val.reception_time}</Td>
-            <Td>{val.current_state}</Td>
-            <Td>
-              <RefundButton>Refund</RefundButton>
-            </Td>
-          </tr>
-        )),
+        CPUFirebase.orderHistory.toReversed().map((val, index) => ({
+          id: index + 1,
+          menu_name: val.menu_name,
+          buyer_name: val.buyer_name,
+          reception_time: val.reception_time,
+          current_state: val.current_state,
+        })),
       );
       unsubscribe = onSnapshot(CPUFirebase.userDocRef, (doc) => {
-        // 나중에 features로 이관할 방법을 찾을 것임
         CPUFirebase.userDoc = doc;
         CPUFirebase.userDocData = doc.data();
         CPUFirebase.orderHistory = JSON.parse(
@@ -214,18 +158,13 @@ export default function CPUHome() {
         setBalance(CPUFirebase.userDocData.balance);
         setKioskImage(CPUFirebase.kioskImageDownloadUrl);
         setOrderList(
-          CPUFirebase.orderHistory.toReversed().map((val, index) => (
-            <tr key={index}>
-              <Td>{index + 1}</Td>
-              <Td>{val.menu_name}</Td>
-              <Td>{val.buyer_name}</Td>
-              <Td>{val.reception_time}</Td>
-              <Td>{val.current_state}</Td>
-              <Td>
-                <RefundButton>Refund</RefundButton>
-              </Td>
-            </tr>
-          )),
+          CPUFirebase.orderHistory.toReversed().map((val, index) => ({
+            id: index + 1,
+            menu_name: val.menu_name,
+            buyer_name: val.buyer_name,
+            reception_time: val.reception_time,
+            current_state: val.current_state,
+          })),
         );
         setIsLoading(false);
       });
@@ -248,7 +187,7 @@ export default function CPUHome() {
     }
     const file = e.target.files[0];
     await CPUFirebase.changeKioskImage(file);
-    setKioskImage(CPUFirebase.kioskImageDownloadUrl); // 이미지 업로드 시 사진 바꾸기
+    setKioskImage(CPUFirebase.kioskImageDownloadUrl);
   };
   return (
     <>
@@ -281,10 +220,10 @@ export default function CPUHome() {
                 <Btn onClick={onAddSellerClick}>부원추가</Btn>
               </HeaderBtns>
             </TopDiv>
-            <BodyDiv>
-              {/* <CoupleList dataList={orderList} /> */}
-              <MenuTable orderList={orderList} />
-            </BodyDiv>
+            <TableWrapper>
+              <MenuTable orderList={orderList} />{" "}
+              {/* Use MenuTable component */}
+            </TableWrapper>
           </CPUHomeBox>
         </Wrapper>
       )}
