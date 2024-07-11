b69ff8633cf59f6460924e04b6d6374ccc9f374d
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-07-11 09:18:12 +0900

    Changed import route

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import KioskThankYou from "./routes/kiosk/kiosk-thankyou";
 import DeveloperHome from "./developer/developer-home";
 import SellerSelect from "./routes/seller/seller-select";
 import SellerHome from "./routes/seller/seller-home";
-import ModeratorHome from "./routes/Moderator/moderator-home";
+import ModeratorHome from "./routes/moderator/moderator-home";
 import LogHome from "./routes/log/log-home";
 
 import AddSeller from "./routes/CPU/add-seller";
