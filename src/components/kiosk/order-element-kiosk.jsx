cba45b7978d65d5b0b8be6053996c78fcb9f430f
Yean Lee <talk2yeanlee@gmail.com>
2024-07-13 20:49:06 +0900

    changed kiosk order element list

--- src/components/kiosk/order-element-kiosk.jsx
+++ src/components/kiosk/order-element-kiosk.jsx
@@ -1,41 +1,43 @@
 import styled from "styled-components";
 import AddQuantity from "../../images/AddQuantity.svg";
 import LowerQuantity from "../../images/LowerQuantity.svg";
-import {
-  BORDER_GRAY,
-  MINSAPAY_FONT /* BUTTON_SHADOW */,
-} from "../theme-definition";
+import { BORDER_GRAY, MINSAPAY_FONT } from "../theme-definition";
 import { useState } from "react";
-// 반응형으로!!
+
 const Wrapper = styled.div`
   width: calc(100% - 0.5vw);
   height: 6vw;
   border: 3px solid ${BORDER_GRAY};
   border-radius: 1.5vw;
-
   display: flex;
   flex-direction: row;
   align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  height: 100%;
+  width: 6vw;
+  border-top-left-radius: calc(1.5vw - 3px);
+  border-bottom-left-radius: calc(1.5vw - 3px);
+  overflow: hidden; /* Ensure contents don't overflow */
 `;
 
 const MenuImage = styled.img`
   height: 100%;
-  aspect-ratio: 1;
-  border-top-left-radius: calc(1.5vw - 3px);
-  border-bottom-left-radius: calc(1.5vw - 3px);
+  width: 100%;
+  object-fit: cover;
 `;
+
 const NoImage = styled.div`
-  height: 13vh;
-  aspect-ratio: 1;
-  font-size: 2vh;
-  border-top-left-radius: calc(1.5vw - 3px);
-  border-bottom-left-radius: calc(1.5vw - 3px);
+  height: 100%;
+  width: 100%;
   display: flex;
-  flex-direction: row;
   align-items: center;
   justify-content: center;
+  font-size: 1vh;
   font-family: ${MINSAPAY_FONT};
   border-right: 3px solid ${BORDER_GRAY};
+  background-color: #f0f0f0; /* Background color to distinguish no image */
 `;
 
 const MenuInfo = styled.div`
@@ -44,17 +46,20 @@ const MenuInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 1.3vh;
+  gap: 1vh;
 `;
+
 const MenuName = styled.div`
   font-size: 1.2vw;
   margin-left: 0.7vw;
 `;
+
 const MenuPrice = styled.div`
   font-size: 1vw;
   color: #2079ff;
   margin-left: 0.7vw;
 `;
+
 const QuantityChange = styled.div`
   width: 3vw;
   height: 100%;
@@ -63,8 +68,9 @@ const QuantityChange = styled.div`
   align-items: center;
   justify-content: space-evenly;
 `;
+
 const Quantity = styled.div`
-  font-size: calc(0.8vw+0.4vh);
+  font-size: calc(0.8vw + 0.4vh);
   font-family: ${MINSAPAY_FONT};
   display: flex;
   flex-direction: column;
@@ -78,7 +84,6 @@ const QuantityChangeImg = styled.img`
 `;
 
 export default function OrderElementKiosk({
-  // 아직 덜만듦 ui도 짜야 하고 여러가지 수량 기능? 등등 추가해야 함
   imageDownloadUrl,
   menuName,
   price,
@@ -89,13 +94,16 @@ export default function OrderElementKiosk({
   onOrderElementKioskDelete,
 }) {
   const [quantityState, setQuantityState] = useState(quantity);
+
   return (
     <Wrapper>
-      {imageDownloadUrl === "" ? (
-        <NoImage>이미지 없음</NoImage>
-      ) : (
-        <MenuImage src={imageDownloadUrl} />
-      )}
+      <ImageContainer>
+        {imageDownloadUrl === "" ? (
+          <NoImage>이미지 없음</NoImage>
+        ) : (
+          <MenuImage src={imageDownloadUrl} />
+        )}
+      </ImageContainer>
       <MenuInfo>
         <MenuName>{menuName}</MenuName>
         <MenuPrice>{price}원</MenuPrice>
@@ -106,7 +114,7 @@ export default function OrderElementKiosk({
           id={id + ".AQ"}
           onClick={(e) => {
             onAddQuantityButtonClick(e);
-            setQuantityState(quantity + 1);
+            setQuantityState(quantityState + 1);
           }}
         />
         <Quantity>{quantityState}</Quantity>
@@ -116,7 +124,7 @@ export default function OrderElementKiosk({
           onClick={(e) => {
             if (quantityState > 1) {
               onLowerQuantityButtonClick(e);
-              setQuantityState(quantity - 1);
+              setQuantityState(quantityState - 1);
             } else if (quantityState === 1) {
               onOrderElementKioskDelete(e);
             }
