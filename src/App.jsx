98ee06e37d093b1ce2e879e24242ff4211ed835c
codeweb404 <codeweb.jcu@gmail.com>
2024-07-14 12:58:16 +0900

    fixed app.jsx import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-import Login from "./routes/login";
+import Login from "./routes/login.jsx";
 import {
   ProtectedBuyer,
   ProtectedCPU,
@@ -12,26 +12,24 @@ import {
   ProtectedSeller,
   ProtectedLog,
   ProtectedLoginSetting,
-} from "./components/protected-routes";
-import { BACKGROUND_GRAY } from "./components/theme-definition";
-import BuyerHome from "./routes/buyer/buyer-home";
-import BuyerPayment from "./routes/buyer/buyer-payment";
-import CPUHome from "./routes/CPU/cpu-home";
-import ChangeMenu from "./routes/CPU/change-menu";
-import KioskHome from "./routes/kiosk/kiosk-home";
-import KioskCover from "./routes/kiosk/kiosk-cover";
-import KioskAuthentiaction from "./routes/kiosk/kiosk-authentication";
-import KioskThankYou from "./routes/kiosk/kiosk-thankyou";
-import DeveloperHome from "./developer/developer-home";
-import SellerSelect from "./routes/seller/seller-select";
-import SellerHome from "./routes/seller/seller-home";
-// import ModeratorHome from "./routes/moderator/moderator-home";
-import ModeratorHome from "./routes/moderator/moderator-home";
-import LogHome from "./routes/log/log-home";
+} from "./components/protected-routes.jsx";
+import { BACKGROUND_GRAY } from "./components/theme-definition.js";
+import BuyerHome from "./routes/buyer/buyer-home.jsx";
+import BuyerPayment from "./routes/buyer/buyer-payment.jsx";
+import CPUHome from "./routes/CPU/cpu-home.jsx";
+import ChangeMenu from "./routes/CPU/change-menu.jsx";
+import KioskHome from "./routes/kiosk/kiosk-home.jsx";
+import KioskCover from "./routes/kiosk/kiosk-cover.jsx";
+import KioskAuthentiaction from "./routes/kiosk/kiosk-authentication.jsx";
+import KioskThankYou from "./routes/kiosk/kiosk-thankyou.jsx";
+import DeveloperHome from "./developer/developer-home.jsx";
+import SellerSelect from "./routes/seller/seller-select.jsx";
+import SellerHome from "./routes/seller/seller-home.jsx";
+import ModeratorHome from "./routes/moderator/moderator-home.jsx";
+import LogHome from "./routes/log/log-home.jsx";
+import AddSeller from "./routes/CPU/add-seller.jsx";
 
-import AddSeller from "./routes/CPU/add-seller";
-
-import ChangePassword from "./routes/login-setting";
+import ChangePassword from "./routes/login-setting.jsx";
 
 const router = createBrowserRouter([
   // 루팅
